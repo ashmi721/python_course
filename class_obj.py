fc599c17143d297b4c13c2ce8409807f1ada77b9
ashmi721 <ashmitak721@gmail.com>
2023-10-30 16:11:15 +0545

    practice some class and objects concepts

--- class_obj.py
+++ class_obj.py
@@ -35,3 +35,35 @@ obj1.display()
 obj1.display_instance_method()
 # call the instance attribute
 obj1.name
+
+
+''' create an class with 2 parameter '''
+class Student:
+  def detail(self,name,address):
+    self.name = name
+    self.address=address
+  def show_detail(self):
+    print (f"Name : {self.name}, Address : {self.address}")
+
+s1 = Student()
+s1.detail("ashmita","Dang")  
+s1.show_detail()
+
+print(" ---- ")
+
+''' create class using constructor '''
+class Employee:
+  def __init__(self,name,age,salary,gender):
+    self.name = name
+    self.age = age
+    self.salary = salary
+    self.gender = gender
+
+  def show_detail(self):
+    print(f"Name of the employee is : {self.name}") 
+    print(f"Age of the employee is : {self.age}") 
+    print("salary of the employee is ",self.salary) 
+    print("Gender of the employee is ",self.gender)
+
+e1 = Employee("Jhon",32,40000,"Male")
+e1.show_detail() 
