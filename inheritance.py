68a813034956c82a663cd2f7cbea60754fbe0ce7
ashmi721 <ashmitak721@gmail.com>
2023-10-30 16:33:52 +0545

    add some inheriatnce related problem

--- inheritance.py
+++ inheritance.py
@@ -30,3 +30,30 @@ circle_area= obj2.calculate_area()
 
 print(f"Area of the rectangle: {rect_area}")
 print(f"Area of the circle: {circle_area}")
+
+print("-----------")
+
+# Base class
+class Vehicle:
+    def __init__(self, cost, engine_type):
+        self.cost = cost
+        self.engine_type = engine_type
+
+    def show_Vehicle(self):
+        print(f"the cost of the vehicle is: {self.cost}")
+        print(f"the engine_type of the vehicle is: {self.engine_type}")
+
+v1 = Vehicle(234000,"Petrol")
+v1.show_Vehicle()
+# Child class
+class Car(Vehicle):
+    def __init__(self, cost, engine_type):
+        super().__init__(cost, engine_type)  # Call the parent class constructor
+        self.car_detail()
+
+    def car_detail(self):
+        print("I am a car")
+
+
+c1 = Car(150000, "Electric")
+c1.show_Vehicle()  # This line will print the vehicle details
